1956486e76a4adedc70a766c586675410a142531
Łukasz Jurczyk <zabszk@protonmail.ch>
2021-09-16 23:01:06 +0200

    Bumped version to 2.0.2 and copyrights

--- Token Validator Windows/Properties/AssemblyInfo.cs
+++ Token Validator Windows/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -9,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SCP:SL Token Validator Windows")]
-[assembly: AssemblyCopyright("Copyright © Łukasz Jurczyk, 2019")]
+[assembly: AssemblyCopyright("Copyright © Łukasz Jurczyk, 2019-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.1")]
-[assembly: AssemblyFileVersion("2.0.1.1")]
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
